de0c435f1463fc02233ee5f72254d0e678827ac1
Maksim Momcilovic <M.Momcilovic.inf20@stud.bbbaden.ch>
2022-12-07 08:12:57 +0100

    .

--- src/components/pages/Projekte.js
+++ src/components/pages/Projekte.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import School from "../../components/inc/SVGS/school";
-import Race from '../../components/inc/SVGS/race'
-import QR from '../../components/inc/SVGS/qr'
+import Race from "../../components/inc/SVGS/race";
+import QR from "../../components/inc/SVGS/qr";
+import Soulchannel from "../inc/SVGS/Website";
+import ThisSite from "../inc/SVGS/Website";
+
 
 export class projekte extends Component {
   render() {
@@ -9,12 +12,14 @@ export class projekte extends Component {
       <div>
         <section
           id="projekt-section"
-          style={{color: "white", backgroundColor: "black"}}
+          style={{ color: "white", backgroundColor: "black" }}
         >
           <div className="container">
             <div className="row pt-5 pb-5 text-end">
               <div className="col-lg-12">
-                <h1 id="skills-title" className="pb-5 display-1">Projekte</h1>
+                <h1 id="skills-title" className="pb-5 display-1">
+                  Projekte
+                </h1>
               </div>
             </div>
             <div className="row justify-content-around align-items-center">
@@ -58,46 +63,74 @@ export class projekte extends Component {
                     </div>
                   </div>
                   <div className="row justify-content-around mt-4 pb-5 text-center">
-                  <div className="col-lg-auto col-md-auto col-sm-auto col-xs-auto">
-                    <a href="https://portfolio.bbbaden.ch/user/m-momcilovic-inf20/virtueller-bbb-rundgang">
-                      <button className="CV-button btn">Portfolio</button>
-                    </a>
-                  </div>
+                    <div className="col-lg-auto col-md-auto col-sm-auto col-xs-auto">
+                      <a href="https://portfolio.bbbaden.ch/user/m-momcilovic-inf20/virtueller-bbb-rundgang">
+                        <button className="CV-button btn">Portfolio</button>
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
               <div className="col-lg-auto col-md-auto col-sm-auto col-xs-auto">
-                <div className="row justify-content-center mb-3 text-center">
-                  <div className="col-lg-auto col-md-auto col-sm-auto col-xs-auto">
-                    <h1 className="project-title display-6">Rennschnecken</h1>
+                <div className="col-lg-auto col-md-auto col-sm-auto col-xs-auto">
+                  <div className="row justify-content-center mb-3 text-center">
+                    <div className="col-lg-auto col-md-auto col-sm-auto col-xs-auto">
+                      <h1 className="project-title display-6">
+                        THE SOUL CHANNEL
+                      </h1>
+                    </div>
                   </div>
-                </div>
-                <div className="row justify-content-center align-items-center">
-                  <div className="col-lg-auto col-md-auto col-sm-auto col-xs-auto">
+                  <div className="row justify-content-center mb-3 text-center">
                     <div className="project">
-                      <a href="https://portfolio.bbbaden.ch/user/m-momcilovic-inf20/rennschnecke-objektorientiert-implementieren-ohne-vererbung">
-                        <Race></Race>
+                      <a href="https://the-soul-channel.de/">
+                        <Soulchannel />
                       </a>
                     </div>
                   </div>
-                  <div className="row justify-content-around mt-4 pb-5 text-center">
-                  <div className="col-lg-auto col-md-auto col-sm-auto col-xs-auto">
-                    <a href="https://portfolio.bbbaden.ch/user/m-momcilovic-inf20/rennschnecke-objektorientiert-implementieren-ohne-vererbung">
-                      <button className="CV-button btn">Portfolio</button>
-                    </a>
+                  <div className="row justify-content-center mb-3 text-center">
+                    <div className="col-lg-auto col-md-auto col-sm-auto col-xs-auto">
+                      <a title="Noch nicht vorhanden..." href="">
+                        <button className="CV-button btn">Portfolio</button>
+                      </a>
+                    </div>
                   </div>
+                </div>
+              </div>
+              <div className="col-lg-auto col-md-auto col-sm-auto col-xs-auto">
+                <div className="col-lg-auto col-md-auto col-sm-auto col-xs-auto">
+                  <div className="row justify-content-center mb-3 text-center">
+                    <div className="col-lg-auto col-md-auto col-sm-auto col-xs-auto">
+                      <h1 className="project-title display-6">
+                        maksim-momcilovic.ch
+                      </h1>
+                    </div>
+                  </div>
+                  <div className="row justify-content-center mb-3 text-center">
+                    <div className="project">
+                      <a title="Führt direkt zum Portfolio, da Sie schon auf der Seite sind! :)" href="https://portfolio.bbbaden.ch/user/m-momcilovic-inf20/maksim-momcilovic-ch">
+                        <ThisSite />
+                      </a>
+                    </div>
+                  </div>
+                  <div className="row justify-content-center mb-3 text-center">
+                    <div className="col-lg-auto col-md-auto col-sm-auto col-xs-auto">
+                      <a href="https://portfolio.bbbaden.ch/user/m-momcilovic-inf20/maksim-momcilovic-ch">
+                        <button className="CV-button btn">Portfolio</button>
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
+
             <div className="row justify-content-center align-items-center text-center">
-            <h1 className="project-title display-6 pt-4">Mehr Projekte folgen...</h1>
-            <ul className="list-unstyled pt-5">
-              <li>Portfolio über diese Website</li>
-              <li>C# WPF Memorygame</li>
-              <li>JQuery Media</li>
-              <li>Handyapplikation</li>
-            </ul>
+              <h1 className="project-title display-6 pt-4">
+                Mehr Projekte folgen...
+              </h1>
+              <ul className="list-unstyled pt-5">
+                <li>C# - WPF Memorygame</li>
+                <li>AwesomeTools - React-Native</li>
+              </ul>
             </div>
           </div>
         </section>
